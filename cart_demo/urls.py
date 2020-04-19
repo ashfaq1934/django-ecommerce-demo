f3ccc93d6187701b2befe8d3a294fa2699228a48
Ashfaq Ahmed <ashfaq_a@live.co.uk>
2020-04-20 00:55:54 +0100

    added quantity attribute and a way to add other attributes of a product

--- cart_demo/urls.py
+++ cart_demo/urls.py
@@ -6,6 +6,6 @@ from product.views import single
 urlpatterns = [
     path('admin/', admin.site.urls),
     re_path(r'^cart/$', view, name='cart'),
-    re_path(r'^cart/(?P<slug>[\w-]+)//(?P<qty>\d+)/$', update_cart, name='update_cart'),
+    re_path(r'^cart/(?P<slug>[\w-]+)/$', update_cart, name='update_cart'),
     re_path(r'^products/(?P<slug>[\w-]+)/$', single, name='single_product')
 ]
