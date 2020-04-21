9f5739b4096f8cf4feb4be62763884460a7fe17e
Ashfaq Ahmed <ashfaq_a@live.co.uk>
2020-04-21 18:20:48 +0100

    added product variations and changed the functionality so that there would be seperate functions for adding an item to the cart or removing it

--- cart/views.py
+++ cart/views.py
@@ -2,20 +2,30 @@ from django.shortcuts import render, HttpResponseRedirect
 from django.urls import reverse
 import json
 from .models import Cart, CartItem
-from product.models import Product
+from product.models import Product, Variation
 
 def view(request):
     try:
-        #create a session for a user's cart
+        #get the session for a user's cart
         the_id = request.session['cart_id']
     except:
         the_id = None
     if the_id:
         #if the session was created and the id was defined and the cart items retrieved, display them
         cart = Cart.objects.get(id=the_id)
+        new_total = 0.00
+        for item in cart.cartitem_set.all():
+            line_total = float(item.product.price) * item.quantity
+            new_total += line_total
+            
+        request.session['items_total'] = cart.cartitem_set.count()
+        #set the cart's total as the new total by gettingthe count
+        cart.total = new_total
+        cart.save()
+        # success message
         context = {"cart": cart}
     else:
-        #otherwise display emoty message
+        #otherwise display empty message
         empty_message = 'Your cart is empty'
         context = {"empty": True, 
                     "empty_message": empty_message}
@@ -23,36 +33,25 @@ def view(request):
     template = 'cart/view.html'
     return render(request, template, context)
 
-def update_cart(request, slug):
+def remove_from_cart(request, id):
+    try:
+        #get the session for a user's cart
+        the_id = request.session['cart_id']
+        cart = Cart.objects.get(id=the_id)
+    except:
+        return HttpResponseRedirect(reverse("cart"))
+    
+    cartitem = CartItem.objects.get(id=id)
+    cartitem.delete()
+    # cartitem.cart = None
+    # cartitem.save()
+    # send success message
+    return HttpResponseRedirect(reverse("cart"))
+
+
+def add_to_cart(request, slug):
     #set expiry time if user doesn't log out
     request.session.set_expiry(120000)
-    try:
-        #get product quantity from the form
-        qty = request.GET.get('qty')
-        update_qty = True
-    except:
-        qty = None
-        update_qty = False
-    
-    note = {}
-    
-    try:
-        colour = request.GET.get('colour')
-        #add colour to notes dictionary
-        note['colour'] = colour
-    except:
-        colour = None
-    
-    try:
-        size = request.GET.get('size')
-        #add size to notes dictionary
-        note['size'] = size
-    except:
-        size = None
-    
-    print(colour)
-    print(size)
-    print(note)
 
     try:
         #update the sessions cart, othetwise create a new cart
@@ -71,30 +70,26 @@ def update_cart(request, slug):
 
     except:
         pass
-    #create the cart item based on the session's cart id and the product chosen
-    cart_item, created = CartItem.objects.get_or_create(cart=cart, product=product)
 
-    if update_qty and qty:
-
-        if int(qty) <= 0:
-            #if the quantity is 0 or less, delete the cart item
-            cart_item.delete()
-        else:
-            #otherwise, set the quantity and save the item
-            cart_item.quantity = qty
-            cart_item.notes = json.dumps(note)
-            cart_item.save()
-    else:
-        pass
+    product_variations = []
     
-    new_total = 0.00
-    for item in cart.cartitem_set.all():
-        line_total = float(item.product.price) * item.quantity
-        new_total += line_total
-    
-    request.session['items_total'] = cart.cartitem_set.count()
-    #set the cart's total as the new total by gettingthe count
-    cart.total = new_total
-    cart.save()
+    if request.method == 'POST':
+        qty = request.POST['qty']
+        for item in request.POST:
+            key = item
+            value = request.POST[key]
+            try:
+                variation = Variation.objects.get(product=product, category__iexact=key, title__iexact=value)
+                product_variations.append(variation)
+            except:
+                pass
 
+        #create the cart item based on the session's cart id and the product chosen
+        cart_item = CartItem.objects.create(cart=cart, product=product)
+        #otherwise, set the quantity and save the item
+        if len(product_variations) > 0:
+                cart_item.variations.add(*product_variations)
+        cart_item.quantity = qty
+        cart_item.save()
+        return HttpResponseRedirect(reverse("cart"))
     return HttpResponseRedirect(reverse("cart"))
